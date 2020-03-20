a7beefe2aa0fe8460bd6a9a04a390e89370b9dc6
henrycutting <henrycutting2000@gmail.com>
2020-03-20 15:18:53 +0000

    first commit

---
+++ source/repos/gitDiff/gitDiff/Compare.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gitDiff
+{
+    class Compare
+    {
+        //simple method to check if both files are the same
+        public static Boolean compare(string textOne, string textTwo)
+        {
+            if (textOne == textTwo)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+
+            }
+        }
+        
+    }
+}
---
+++ source/repos/gitDiff/gitDiff/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace gitDiff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//asks which files are to be compared
+            Console.WriteLine("Which files would you like to compare");
+            Console.WriteLine("1: 1a and 1b");
+            Console.WriteLine("2: 2a and 2b");
+            Console.WriteLine("3: 3a and 3b");
+            string input = Console.ReadLine();
+//switch statement determines from user input which files are being compared           
+            switch (Convert.ToInt32(input))
+            {
+                case 1:
+                    //reads the text files then calls the compare function
+                    string textOne = File.ReadAllText("GitRepositories_1a.txt");
+                    string textTwo = File.ReadAllText("GitRepositories_1b.txt");
+                    if (Compare.compare(textOne, textTwo))
+                    {
+                        Console.WriteLine("They are the same");
+                    }
+                    else
+                    {
+                        Console.WriteLine("They are not the same");
+                    }
+                    
+                    break;
+                case 2:
+                    string textThree = File.ReadAllText("GitRepositories_2a.txt");
+                    string textFour = File.ReadAllText("GitRepositories_2b.txt");
+                    if (Compare.compare(textThree, textFour))
+                    {
+                        Console.WriteLine("They are the same");
+                    }
+                    else
+                    {
+                        Console.WriteLine("They are not the same");
+                    }
+                    break;
+                case 3:
+                    string textFive = File.ReadAllText("GitRepositories_3a.txt");
+                    string textSix = File.ReadAllText("GitRepositories_3b.txt");
+                    if (Compare.compare(textFive, textSix))
+                    {
+                        Console.WriteLine("They are the same");
+                    }
+                    else
+                    {
+                        Console.WriteLine("They are not the same");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Incorrect input");
+                    break;
+
+            }
+           
+
+        }
+    }
+}
